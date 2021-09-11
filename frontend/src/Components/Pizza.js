cc595d25bbc58810c22d3060d7ee93cd3f5c448e
Raunak173 <raunakagarwal173@gmail.com>
2021-09-11 16:40:20 +0530

    added modal

--- frontend/src/Components/Pizza.js
+++ frontend/src/Components/Pizza.js
@@ -7,10 +7,26 @@ import {
   TextField,
   MenuItem,
   Button,
+  Modal,
 } from "@material-ui/core";
 import React, { useState } from "react";
 
-const useStyles = makeStyles({
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
   card: {
     width: "25%",
     margin: "2%",
@@ -46,65 +62,109 @@ const useStyles = makeStyles({
     width: "100%",
     marginTop: "5%",
   },
-});
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
 
 function Pizza({ pizza }) {
   const classes = useStyles();
   const [qty, setQty] = useState(1);
   const [varient, setVarient] = useState("small");
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <img src={pizza.image} height="300px" width="320px" />
+      <p id="simple-modal-description">{pizza.description}</p>
+    </div>
+  );
+
   return (
-    <Card className={classes.card}>
-      <CardHeader title={pizza.name} className={classes.title} />
-      <CardMedia>
-        <img src={pizza.image} height="250px" width="280px" />
-      </CardMedia>
-      <div className={classes.container}>
-        <TextField
-          className={classes.field}
-          id="standard-select-currency"
-          select
-          label="Variants"
-          value={varient}
-          onChange={(e) => {
-            setVarient(e.target.value);
-          }}
-        >
-          {pizza.varients.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          className={classes.field}
-          id="standard-select-currency"
-          select
-          label="Quantity"
-          value={qty}
-          onChange={(e) => {
-            setQty(e.target.value);
-          }}
-        >
-          {[...Array(10).keys()].map((x, i) => (
-            <MenuItem key={i + 1} value={i + 1}>
-              {i + 1}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-      <div className={classes.container2}>
-        <div>
-          <h4>{pizza.prices[0][varient] * qty} Rs/-</h4>
+    <>
+      <Card className={classes.card}>
+        <CardHeader
+          title={pizza.name}
+          className={classes.title}
+          onClick={handleOpen}
+        />
+        <CardMedia>
+          <img
+            src={pizza.image}
+            height="250px"
+            width="280px"
+            onClick={handleOpen}
+          />
+        </CardMedia>
+        <div className={classes.container}>
+          <TextField
+            className={classes.field}
+            id="standard-select-currency"
+            select
+            label="Variants"
+            value={varient}
+            onChange={(e) => {
+              setVarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            className={classes.field}
+            id="standard-select-currency"
+            select
+            label="Quantity"
+            value={qty}
+            onChange={(e) => {
+              setQty(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => (
+              <MenuItem key={i + 1} value={i + 1}>
+                {i + 1}
+              </MenuItem>
+            ))}
+          </TextField>
         </div>
-        <Button
-          variant="contained"
-          className={classes.btn}
-          style={{ backgroundColor: "#e71d36", color: "white" }}
-        >
-          Add to cart
-        </Button>
-      </div>
-    </Card>
+        <div className={classes.container2}>
+          <div>
+            <h4>{pizza.prices[0][varient] * qty} Rs/-</h4>
+          </div>
+          <Button
+            variant="contained"
+            className={classes.btn}
+            style={{ backgroundColor: "#e71d36", color: "white" }}
+          >
+            Add to cart
+          </Button>
+        </div>
+      </Card>
+      <Modal open={open} onClose={handleClose}>
+        {body}
+      </Modal>
+    </>
   );
 }
 
