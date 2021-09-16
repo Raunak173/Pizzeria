ab4ce791e8153513411fa97eb486d9a8c3e0edf9
Raunak173 <raunakagarwal173@gmail.com>
2021-09-16 17:14:24 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,11 @@ app.use("/api/orders/", ordersRoute);
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
   });
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
